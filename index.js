f710c47588e1be2b7c13d9bc11b0b9b1c81e6478
German Andres Ortiz Martinez <german.andres201@gmail.com>
2024-11-18 20:35:17 -0500

    Creando los endPoints a utilizar

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello Germán Node.js</h1>')
 })
 
+app.post('/login', (req, res) => {})
+app.post('/register', (req, res) => {})
+app.post('/logout', (req, res) => {})
+
+app.post('/protected', (req, res) => {})
+
 app.listen(PORT, () => {
   console.log(`Server runnig on port ${PORT}`)
 })
